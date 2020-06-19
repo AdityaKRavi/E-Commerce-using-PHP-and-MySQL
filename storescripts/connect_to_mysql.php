d5e9168d5859c86ee46651a58419b7289d251744
AdityaKRavi <ravi981957632@gmail.com>
2020-06-19 10:06:05 +0530

    Update connect_to_mysql.php

--- storescripts/connect_to_mysql.php
+++ storescripts/connect_to_mysql.php
@@ -2,9 +2,9 @@
 error_reporting(E_ALL ^ E_DEPRECATED);
 ?>
 <?php   
-$db_host = "localhost"; 
+$db_host = "mysqldb.qa.ecom-app.uniphy.uniphytest.com"; 
 $db_username = "root";   
-$db_pass = "";  
+$db_pass = "root";  
 $db_name = "mystore"; 
  
 mysql_connect("$db_host","$db_username","$db_pass") or die ("could not connect to mysql");
